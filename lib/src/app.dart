f772aaa91f44536b9c22c8d75176a4ad0b346de3
JoeM-RP <JMeyer@rightpoint.com>
2021-03-18 11:50:18 -0500

    Initial auth screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'blocs/provider.dart';
 
 const title = 'Demo App';
 
 class App extends StatelessWidget {
   build(context) {
-    return MaterialApp(
-      title: 'Code Walk Demo',
-      home: Scaffold(
-        body: Container(),
+    return Provider(
+      child: MaterialApp(
+        title: 'Code Walk Demo',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
       ),
     );
   }
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,79 @@
+import 'package:demo/src/app.dart';
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
+
+class LoginScreen extends StatelessWidget {
+  // Main Widget
+  Widget build(context) {
+    final bloc = Provider.of(context);
+
+    return Container(
+        margin: EdgeInsets.all(28.0),
+        child: Column(children: [
+          titleField(),
+          emailField(bloc),
+          passwordField(bloc),
+          Container(margin: EdgeInsets.only(top: 28.0)),
+          submitButton(bloc),
+        ]));
+  }
+
+  // Title Field
+  Widget titleField() {
+    return Text(
+      'Hello, Code Walk!',
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+    );
+  }
+
+  // Email Field
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+        stream: bloc.email,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changeEmail,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                hintText: 'you@example.com',
+                labelText: 'Email Address',
+                errorText: snapshot.error),
+          );
+        });
+  }
+
+  // Password Field
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+        stream: bloc.password,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changePassword,
+            obscureText: false,
+            decoration: InputDecoration(
+                hintText: 'Password',
+                labelText: 'Password',
+                errorText: snapshot.error),
+          );
+        });
+  }
+
+  // Log In Button
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+        stream: bloc.submitValid,
+        builder: (context, snapshot) {
+          return ButtonTheme(
+            minWidth: double.infinity,
+            child: RaisedButton(
+              child: Text('Log In'),
+              color: Colors.blue,
+              textColor: Colors.white,
+              onPressed: snapshot.hasData ? bloc.submit() : null,
+            ),
+          );
+        });
+  }
+}
