306a36a84f733a12ead7bdb852ea6c040d3e7601
JoeM-RP <JMeyer@rightpoint.com>
2021-03-18 11:23:03 -0500

    Reset

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
-import 'blocs/provider.dart';
 
-const title = 'Log Me In';
+const title = 'Demo App';
 
 class App extends StatelessWidget {
   build(context) {
-    return Provider(
-      child: MaterialApp(
-        title: 'Log Me In',
-        home: Scaffold(
-          body: LoginScreen(),
-        ),
+    return MaterialApp(
+      title: 'Code Walk Demo',
+      home: Scaffold(
+        body: Container(),
       ),
     );
   }
